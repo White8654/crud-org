c483527d0934643026464d3b41b01fee5d6174a5
SOHAN <108630063+White8654@users.noreply.github.com>
2024-10-16 11:44:33 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const { withSentryConfig } = require('@sentry/nextjs');
 const nextConfig = {};
 
-const moduleExports = {
-    // Your existing Next.js config here
-  };
-  
-  const sentryWebpackPluginOptions = {
-    // Additional config options for the Sentry Webpack plugin.
-    silent: true, // Suppresses all logs
-  };
-  
-  module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+export default nextConfig;
